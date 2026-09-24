80e2b457126b619162584bd22b956fe4691ec396
ilysouza <ilysouza@outlook.com>
2026-09-24 11:14:29 -0300

    Terminando tabela do dashboard e imp. admin

---
+++ app/Livewire/Admin/AdminIndex.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use App\Models\User;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class AdminIndex extends Component
+{
+    use WithPagination;
+    
+    public $pesquisa = '';
+    public $situacao = 'ativos';
+
+    public function administradores(){
+        // Filtro por tipo de usuário
+        $admins = User::where('tipo', User::TIPO_ADMIN);
+
+        // onlyTrashed retorna somente os excluidos, withTrashed retorna todos.
+        if($this->situacao == 'excluidos'){
+            $admins->onlyTrashed();
+        } else if ($this->situacao == 'todos') {
+            $admins->withTrashed();
+        }
+
+        $admins->where('nome', 'like', '%' . $this->pesquisa . '%')->orWhere('email', 'like', '%' . $this->pesquisa . '%');
+
+        return $admins->latest()->paginate(10);
+    }
+
+
+    public function render()
+    {
+        return view('livewire.admin.admin-index')-> layout('layouts.app', ['title'=>'Administradores', 'admin' => true]);
+    }
+}
--- resources/views/livewire/dashboard/index.blade.php
+++ resources/views/livewire/dashboard/index.blade.php
@@ -62,11 +62,6 @@
             </div>
 
             <div class="card-body">
-                @if($ultimosProdutos->isEmpty())
-                    <p class="text-secondary mb-0">
-                        Nenhum produto cadastrado
-                    </p>
-                @else
                 
                 <div class="table-responsive">
                     <table class="table table-hover align-middle mb-0">
@@ -85,6 +80,18 @@
                                 <tr>
                                     <td>{{ $produto->nome }}</td>
                                     <td>{{ $produto->categoria->nome }}</td>
+                                    <td>{{ $produto->preco }}</td>
+                                    <td>
+                                        <span class="badge {{ $produto->ativo ? 'text-bg-success' : 'text-bg-secondary'}}">
+                                            {{ $produto->ativo? 'Ativo' : 'Inativo' }}
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <span class="badge {{ $produto->destaque ? 'text-bg-danger' : 'text-bg-light'}}">
+                                            {{ $produto->destaque ? 'Sim' : 'Não' }}
+                                        </span>
+                                    </td>
+                                    <td>{{ $produto->created_at->format('d/m/Y H:i') }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
